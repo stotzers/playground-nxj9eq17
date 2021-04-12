0bcb465f42a2a18a2bac8b6de66b8eaa401762c8
stotzers <82386922+stotzers@users.noreply.github.com>
2021-04-12 16:34:36 +0200

    Update server.js

--- nodejs-project/server.js
+++ nodejs-project/server.js
@@ -39,9 +39,7 @@ server.post('/login', function (req, res) {
     } else if (!row) {
       res.redirect("/index.html#unauthorized");
     } else {
-      res.send('Bonjour, <b>' + row.username + '</b> Vous êtes connecté en tant que <b>' + row.name + '<br /><br />');
-      res.send('Bonjour, <b>' + row.name + '</b><br /><a href="/index.html">Retour à la page d\'accueil</a>');
-    }
+      res.send('Bonjour, <b>' + row.username + '</b> Vous êtes connecté en tant que <b>' + row.name + '<br /><br /><br /><a href="/index.html">Retour à la page d\'accueil</a>');
   });
 
 });
