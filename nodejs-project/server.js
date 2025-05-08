700f11087aade8fcce60782ef43e6a91d224a615
stotzers <82386922+stotzers@users.noreply.github.com>
2025-05-08 03:43:40 +0200

    Update server.js

--- nodejs-project/server.js
+++ nodejs-project/server.js
@@ -44,8 +44,8 @@ server.post('/login', function (req, res) {
         msg = 'Bonjour <b>' + row.username + ',</b><br /> Vous êtes connecté en tant que <b>' + row.role + '</b>';
       }
     }
-    res.send(msg + '<br /><br /><br /><a href="/index.html">Déconnexion et retour à la page d\'accueil</a> <br /><br /><br /><br />'
-		+'<div id="myConsole" style="font-family:Courier,monospace;background-color: #ffffff;margin:10px;padding: 10px;width: 90%;height: 200px;border: 2px solid #ccc;overflow-y:auto;font-size: 18px;">'
+    res.send(msg + '<br /><br /><br /><br /><br /><a href="/index.html">Déconnexion et retour à la page d\'accueil</a> <br /><br />'
+		+'<div id="myConsole" style="font-family:Courier,monospace;background-color: #ffffff;margin:10px;padding: 10px;width: 95%;height: 100px;border: 2px solid #ccc;overflow-y:auto;font-size: 18px;">'
 		+'Votre requête à la base de données: <br/><b>' + query + '</b></div>');
   });
 });
